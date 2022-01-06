e931bcb4ba9216da8301ef260b9b9a93b73adcbc
heweijian4933 <mrfangbianmian@163.com>
2022-01-06 16:29:01 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const Koa = require('koa')
+const app = new Koa()
+
+app.use((ctx, next) => {
+    ctx.body = 'hello world'
+})
+app.listen(3000, () => {
+    console.log('admin server is running at port 3000');
+})
